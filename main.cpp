5b56cc428a7dd3609281af43631dfbe67c92b0d3
svang56 <svang56@mail.csuchico.edu>
2018-02-28 17:18:18 -0800

    Stubborn Player

--- main.cpp
+++ main.cpp
@@ -6,6 +6,27 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int userinput = 0;
+  int GameWonNum = 0;
+  int gamestartnum = 0;
+
+  cout<<"How many times do you want to play Door Prize?\n";
+  cin>>userinput;
+ 
+    while(gamestartnum <= userinput)
+    {
+        game.newGame();
+        game.guessDoorC();
+        game.guessDoorC();
+        if(game.isWinner() == true)
+        {
+            GameWonNum++;
+        }
+        gamestartnum++;
+
+    }
+    cout<<"You won "<<GameWonNum<<" games."<<endl;
+
+  
   return 0;
 }
